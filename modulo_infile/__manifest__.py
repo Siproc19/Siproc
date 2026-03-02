cbbf15ec6cbfa4e5de01e56c9d420e777ba0b95a
DiederichSolis <diederich.josue.emidio@gmail.com>
2026-03-01 20:39:22 -0600

    feat: v19

--- modulo_infile/__manifest__.py
+++ modulo_infile/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "SIPROC FEL INFILE Guatemala",
-    "version": "17.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Ronald de León / SIPROC",
     "website": "https://siproc.com",
     "license": "LGPL-3",
