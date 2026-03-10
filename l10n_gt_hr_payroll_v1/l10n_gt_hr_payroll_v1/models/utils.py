17e73fc6f9a1d93254ba598cd09667a5c974c271
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-09 23:35:37 -0600

    Delete l10n_gt_hr_payroll_v1/l10n_gt_hr_payroll_v1 directory

--- l10n_gt_hr_payroll_v1/l10n_gt_hr_payroll_v1/models/utils.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def safe_div(numerator, denominator):
-    return numerator / denominator if denominator else 0.0
-
-
-def round_gt(amount, precision=2):
-    return round(amount or 0.0, precision)
