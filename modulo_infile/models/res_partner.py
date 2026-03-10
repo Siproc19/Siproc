b30bf5a6acff215f262c349fef02daf1f727ceea
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-09 22:14:23 -0600

    Add CUI field and consultation methods to ResPartner

--- modulo_infile/models/res_partner.py
+++ modulo_infile/models/res_partner.py
@@ -1,4 +1,28 @@
-from odoo import models
+from odoo import models, fields
+from odoo.exceptions import UserError
+
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
+
+    cui = fields.Char(string="CUI / DPI")
+
+    def action_consultar_nit(self):
+        for partner in self:
+            if not partner.vat:
+                raise UserError("Ingrese un NIT para consultar")
+
+            data = self.env["fel.service"].consultar_nit(partner.vat)
+
+            if data.get("nombre"):
+                partner.name = data["nombre"]
+
+    def action_consultar_cui(self):
+        for partner in self:
+            if not partner.cui:
+                raise UserError("Ingrese un DPI / CUI")
+
+            data = self.env["fel.service"].consultar_cui(partner.cui)
+
+            if data.get("nombre"):
+                partner.name = data["nombre"]
