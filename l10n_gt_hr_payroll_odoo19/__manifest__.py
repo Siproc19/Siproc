9553bd04e2c2d51d827aa9a3f738c25808b25759
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-11 23:21:51 -0600

    Update __manifest__.py

--- l10n_gt_hr_payroll_odoo19/__manifest__.py
+++ l10n_gt_hr_payroll_odoo19/__manifest__.py
@@ -1,17 +1,22 @@
 {
-    "name": "Guatemala Payroll Core (Odoo 19)",
+    "name": "Planilla Guatemala (Odoo 19)",
     "version": "19.0.1.0.0",
-    "author": "GT Localization",
+    "author": "SIPROC",
     "category": "Human Resources",
     "depends": [
         "hr",
         "hr_payroll",
         "hr_holidays",
         "hr_attendance",
+        "account",
     ],
     "data": [
+        "security/security.xml",
         "security/ir.model.access.csv",
+
         "views/hr_employee_views.xml",
+        "views/hr_version_views.xml",
+        "views/res_company_views.xml",
         "views/menu.xml",
     ],
     "installable": True,
