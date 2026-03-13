fd8f7632c2ca6d3a240903d147a0c6e218223c3d
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-13 17:03:55 -0600

    Update __manifest__.py

--- siproc_delivery_logistics/__manifest__.py
+++ siproc_delivery_logistics/__manifest__.py
@@ -35,6 +35,8 @@ Módulo de logística de entregas para Odoo 19.
     ],
     "assets": {
         "web.assets_backend": [
+            "siproc_delivery_logistics/static/lib/leaflet/leaflet.css",
+            "siproc_delivery_logistics/static/lib/leaflet/leaflet.js",
             "siproc_delivery_logistics/static/src/js/gps_tracker.js",
             "siproc_delivery_logistics/static/src/js/delivery_map_action.js",
             "siproc_delivery_logistics/static/src/xml/delivery_map_templates.xml",
