cb6f0802fa54879c4378c2836529db38cba84b9b
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-14 22:30:02 -0600

    Add files via upload

--- l10n_gt_hr_payroll_odoo19/__manifest__.py
+++ l10n_gt_hr_payroll_odoo19/__manifest__.py
@@ -1,12 +1,12 @@
 
 {
-"name": "GT Payroll Base",
-"version": "19.0.1.0",
-"depends": ["hr","hr_contract","mail"],
-"author": "SIPROC",
-"category": "Human Resources",
-"summary": "Planilla Guatemala base",
-"data": [],
-"installable": True,
-"application": True
+    "name": "GT Payroll Base",
+    "version": "19.0.1.0",
+    "author": "SIPROC",
+    "category": "Human Resources",
+    "depends": ["base", "mail", "hr"],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
 }
--- siproc_sales_bonus/__manifest__.py
+++ siproc_sales_bonus/__manifest__.py
@@ -1,12 +1,12 @@
 
 {
-"name": "SIPROC Sales Bonus",
-"version": "19.0.1.0",
-"depends": ["base","mail","sale_management"],
-"author": "SIPROC",
-"category": "Sales",
-"summary": "Bonificaciones comerciales SIPROC",
-"data": [],
-"installable": True,
-"application": True
+    "name": "SIPROC Sales Bonus",
+    "version": "19.0.1.0",
+    "author": "SIPROC",
+    "category": "Sales",
+    "depends": ["base", "mail", "sale_management"],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
 }
