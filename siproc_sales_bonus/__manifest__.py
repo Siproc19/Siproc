cd0ac55ddf198ac8dc25311a28fd4b0a88c473ea
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-14 22:18:58 -0600

    Add files via upload

---
+++ siproc_sales_bonus/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+"name": "SIPROC Sales Bonus",
+"version": "19.0.1.0",
+"depends": ["base","mail","sale_management"],
+"author": "SIPROC",
+"category": "Sales",
+"summary": "Bonificaciones comerciales SIPROC",
+"data": [],
+"installable": True,
+"application": True
+}
