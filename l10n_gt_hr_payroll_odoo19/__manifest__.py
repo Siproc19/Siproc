55a099a44282f61c29845809cdfec864bb53089b
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-15 18:27:22 -0600

    Update __manifest__.py

--- l10n_gt_hr_payroll_odoo19/__manifest__.py
+++ l10n_gt_hr_payroll_odoo19/__manifest__.py
@@ -1,30 +1,34 @@
 {
     "name": "GT Payroll Base",
-    "version": "19.0.2.1",
-    "summary": "Planilla Guatemala base para Odoo 19",
-    "description": '''
-Planilla Guatemala base para Odoo 19.
-Incluye parámetros, períodos, líneas de planilla y cálculo básico.
-    ''',
+    "version": "19.0.1.0.0",
     "author": "SIPROC",
-    "website": "https://www.siprocgt.com",
-    "category": "Human Resources/Payroll",
-    "license": "LGPL-3",
+    "category": "Human Resources",
     "depends": [
-        "base",
-        "mail",
         "hr",
-        "hr_contract"
+        "hr_payroll",
+        "hr_holidays",
+        "hr_attendance",
+        "account",
+        "mail",
     ],
     "data": [
         "security/security.xml",
         "security/ir.model.access.csv",
+
         "data/sequence.xml",
-        "views/menu.xml",
-        "views/gt_payroll_parameter_views.xml",
+        "data/work_entry_types.xml",
+        "data/salary_rule_parameters.xml",
+
         "views/hr_employee_views.xml",
-        "views/payroll_run_views.xml"
+        "views/hr_version_views.xml",
+        "views/hr_leave_views.xml",
+        "views/hr_payslip_views.xml",
+        "views/payroll_parameter_views.xml",
+        "views/overtime_views.xml",
+        "views/liquidation_views.xml",
+        "views/res_company_views.xml",
+        "views/menu.xml",
     ],
     "installable": True,
-    "application": True
+    "application": True,
 }
