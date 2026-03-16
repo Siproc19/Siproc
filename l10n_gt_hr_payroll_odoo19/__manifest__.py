93134cbf9b0c569f3a66e8f54db0f05b8ce698e8
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-03-15 19:20:23 -0600

    Update __manifest__.py

--- l10n_gt_hr_payroll_odoo19/__manifest__.py
+++ l10n_gt_hr_payroll_odoo19/__manifest__.py
@@ -1,34 +1,74 @@
+# -*- coding: utf-8 -*-
 {
     "name": "GT Payroll Base",
-    "version": "19.0.1.0.0",
+    "summary": "Base de planilla para Guatemala",
+    "description": """
+        Módulo base de planilla para Guatemala.
+        Incluye:
+        - Parámetros de planilla
+        - Horas extra
+        - Liquidaciones
+        - Extensiones de empleados
+        - Extensiones de payslip
+    """,
     "author": "SIPROC",
+    "website": "https://siprocgt.com",
     "category": "Human Resources",
+    "version": "19.0.1.0.0",
+    "license": "LGPL-3",
+
     "depends": [
+        "base",
+        "mail",
         "hr",
         "hr_payroll",
         "hr_holidays",
         "hr_attendance",
         "account",
-        "mail",
     ],
+
     "data": [
+
+        # ----------------
+        # SEGURIDAD
+        # ----------------
         "security/security.xml",
         "security/ir.model.access.csv",
 
+        # ----------------
+        # DATA
+        # ----------------
         "data/sequence.xml",
         "data/work_entry_types.xml",
         "data/salary_rule_parameters.xml",
 
+        # ----------------
+        # VISTAS HR
+        # ----------------
         "views/hr_employee_views.xml",
         "views/hr_version_views.xml",
         "views/hr_leave_views.xml",
         "views/hr_payslip_views.xml",
+
+        # ----------------
+        # MODELOS GT
+        # ----------------
         "views/payroll_parameter_views.xml",
         "views/overtime_views.xml",
         "views/liquidation_views.xml",
+
+        # ----------------
+        # EMPRESA
+        # ----------------
         "views/res_company_views.xml",
+
+        # ----------------
+        # MENÚ
+        # ----------------
         "views/menu.xml",
     ],
+
     "installable": True,
     "application": True,
+    "auto_install": False,
 }
