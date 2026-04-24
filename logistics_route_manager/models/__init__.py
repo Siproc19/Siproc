7184825942b4e719ca4785cc517aac03bd7630f2
Siproc <162186757+Siproc19@users.noreply.github.com>
2026-04-24 05:49:33 -0600

    Delete logistics_route_manager directory

--- logistics_route_manager/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import logistics_location
-from . import logistics_vehicle
-from . import logistics_driver
-from . import logistics_task
-from . import logistics_route
-from . import res_config_settings
--- logistics_route_manager/static/src/js/route_optimizer.js
+++
@@ -1,3 +0,0 @@
-/** @odoo-module **/
-// Intencionalmente minimal para Odoo 19. La optimización vive en el backend.
-export function optimizeRouteClientSide() { return null; }
